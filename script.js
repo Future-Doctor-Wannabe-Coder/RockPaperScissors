2f0174f51db68a6151c14618dee666d9e0fb4613
Future-Doctor-Wannabe-Coder <zcm102297@gmail.com>
2023-07-09 12:42:56 -0400

    fixing game function

--- script.js
+++ script.js
@@ -36,43 +36,108 @@ function playerSelection() {
 
 
 
+
 function playRound(playerChoice, computerChoice) {
 
     if (playerChoice === "rock") {
         if (computerChoice === "scissors") {
-            return "You won this round!";
+            console.log("Rock beats scissors! You won this round!");
+            return "Rock beats scissors! You won this round!";
         }
         else if (computerChoice === "paper") {
-            return "Computer won this round!";
+            console.log("Paper beats rock! Computer won this round!");
+            return "Paper beats rock! Computer won this round!";
         }
         else {
-            return "This round is a tie."
+            console.log("Both selected Rock. This round is a tie.");
+            return "Both selected Rock. This round is a tie.";
         }
     }
 
     else if (playerChoice === "paper") {
         if (computerChoice === "scissors") {
-            return "Computer won this round!";
+            console.log("Scissors beats paper! Computer won this round!");
+            return "Scissors beats paper! Computer won this round!";
         }
         else if (computerChoice === "paper") {
-            return "This round is a tie.";
+            console.log("Both selected paper. This round is a tie.");
+            return "Both selected paper. This round is a tie.";
         }
         else {
-            return "You won this round!"
+            console.log("Paper beats rock! You won this round!");
+            return "Paper beats rock! You won this round!";
         }
     }
 
     else if (playerChoice === "scissors") {
         if (computerChoice === "scissors") {
-            return "This round is a tie.";
+            console.log("Both selected scissors. This round is a tie.");
+            return "Both selected scissors. This round is a tie.";
         }
         else if (computerChoice === "paper") {
-            return "You won this round!";
+            console.log("Scissors beats paper! You won this round!");
+            return "Scissors beats paper! You won this round!";
         }
         else {
-            return "Computer won this round!"
+             console.log("Rock beats scissors! Computer won this round!");
+             return "Rock beats scissors! Computer won this round!";
         }
     }
 }
 
-console.log(playRound(playerSelection(), computerSelection()));
+
+
+function game() {
+
+    let comWins = 0;
+        let userWins = 0;
+        let ties = 0;
+        let rounds = 0;
+
+    for (let i = 0; i < 5; i++) {
+        result = playRound(playerSelection(), computerSelection());
+        console.log(result);
+        rounds++;
+
+
+        if (result == "Rock beats scissors! Computer won this round!" || result == "Scissors beats paper! Computer won this round!" || result == "Paper beats rock! Computer won this round!") {
+            comWins++;
+            console.log(rounds);
+            console.log(comWins);
+            console.log(userWins);
+            console.log(ties);
+
+
+
+
+            
+        }
+
+        else if (result == "Scissors beats paper! You won this round!" || result == "Paper beats rock! You won this round!" || result == "Rock beats scissors! You won this round!") {
+            userWins++;
+            console.log(rounds);
+            console.log(comWins);
+            console.log(userWins);
+            console.log(ties);
+
+        }
+
+        else {
+            ties++;
+            console.log(rounds);
+            console.log(comWins);
+            console.log(userWins);
+            console.log(ties);
+        }
+    }
+
+}
+
+game();
+
+//So, everything is working except for the game function. Whenever we attempt to get a result
+//from a round using if/else statements or a for loop, the iteration only occurs within that
+//local scope, so things like UserWins or ComWins don't change once the loop is finished. This
+//effectively results in every game being a tie. We also can't put the if/else statement within
+//the other statments because then every round would return a game result. I really can't figure
+//out how to get around this.
